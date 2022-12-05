4618bec15747538e9ab920eb65023f12613ae96a
Fasan_Oluwabukola <oluwabukolafasan@gmail.com>
2022-12-05 18:41:00 +0000

    signUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -15,27 +18,65 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text("My Registration Page"),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      body: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Container(
+            child: Column(children: [
+              SizedBox(height: 50),
+              Text("Sign Up",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+              SizedBox(height: 20),
+              TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "First Name",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5)))),
+              SizedBox(height: 30),
+              TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Last Name",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5)))),
+              SizedBox(height: 30),
+              TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Email",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5)))),
+              SizedBox(height: 30),
+              TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Password",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5)))),
+              Container(
+                margin: EdgeInsets.all(25),
+                child: FlatButton(
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(fontSize: 30.0),
+                    ),
+                    color: Colors.blueAccent,
+                    textColor: Colors.white,
+                    onPressed: () {}),
+              ),
+            ]),
+          )),
     );
   }
 }
